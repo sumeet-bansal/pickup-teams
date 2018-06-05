3c4bd72e9f61e02902f4930c1a8c4a4a0a05aeb6
Arnold Duan <c3duan@ucsd.edu>
2018-06-05 10:33:03 -0700

    Finished Profiling

--- routes/profile.js
+++ routes/profile.js
@@ -8,9 +8,10 @@ MongoClient.connect('mongodb://Arnold:123456@ds231090.mlab.com:31090/pickup-team
   db = client.db('pickup-teams');
 });
 
-router.get('/profile/:name', (req, res) => {
-  var name = req.params.name;
-  db.collection('users').find({user_name: name}).toArray(function(err, result) {
+router.get('/', (req, res) => {
+  //var name = req.params.name;
+  var ObjectId = require('mongodb').ObjectID;
+  db.collection('users').find({"_id" : new ObjectId("5b15e7215b79b4e368b50371")}).toArray(function(err, result) {
     if (err) return console.log(err);
     res.render('profile.hbs', result[0])
   });
